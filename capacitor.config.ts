bdb73c2ce7e5614dcd24c098ef70029a84499f97
ED-Pablo <espinozapablouets@gmail.com>
2024-01-26 07:33:10 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Integracion_APIREST',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
